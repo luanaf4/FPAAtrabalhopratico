ab7ce9c8b65f15e901e51bab3370f333f539a2e3
luanaf4 <luana.fleury4@gmail.com>
2023-12-12 02:04:29 -0300

    backtracking atualização

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,43 +1,65 @@
 import java.util.Arrays;
 
 public class Backtracking {
+    private int numCaminhoes;
     private int[] rotas;
     private int[] caminhoes;
-    private int melhorDiferenca = Integer.MAX_VALUE;
+    private int minDiferenca = Integer.MAX_VALUE;
+    private int mediaKm;
 
-    public Backtracking(int[] rotas, int numCaminhoes) {
+    public Backtracking(int numCaminhoes, int[] rotas) {
+        this.numCaminhoes = numCaminhoes;
         this.rotas = rotas;
         this.caminhoes = new int[numCaminhoes];
+        this.mediaKm = Arrays.stream(rotas).sum() / numCaminhoes;
     }
 
-    public void distribuir() {
+    public void distribuirRotas() {
         Arrays.sort(rotas);
-        backtrack(rotas.length - 1);
+        if (Arrays.stream(rotas).sum() % numCaminhoes == 0) {
+            backtrack(0, true);
+        } else {
+            System.out.println("Não foi possível dividir igualmente. Tentando minimizar a diferença...");
+            backtrack(0, false);
+        }
     }
 
-    private void backtrack(int rotaAtual) {
-        if (rotaAtual < 0) {
+    private void backtrack(int rotaAtual, boolean dividirIgualmente) {
+        if (rotaAtual == rotas.length) {
             int maxKm = Arrays.stream(caminhoes).max().getAsInt();
             int minKm = Arrays.stream(caminhoes).min().getAsInt();
             int diferenca = maxKm - minKm;
 
-            if (diferenca < melhorDiferenca) {
-                melhorDiferenca = diferenca;
+            if (diferenca < minDiferenca) {
+                minDiferenca = diferenca;
                 System.out.println("Nova melhor distribuição: " + Arrays.toString(caminhoes));
             }
             return;
         }
 
-        for (int i = 0; i < caminhoes.length; i++) {
+        for (int i = 0; i < numCaminhoes; i++) {
             caminhoes[i] += rotas[rotaAtual];
-            backtrack(rotaAtual - 1);
+
+            if (dividirIgualmente && caminhoes[i] <= mediaKm || !dividirIgualmente && isPromising(i)) {
+                backtrack(rotaAtual + 1, dividirIgualmente);
+            }
+
             caminhoes[i] -= rotas[rotaAtual];
         }
     }
 
+    private boolean isPromising(int caminhao) {
+        int maxKm = Arrays.stream(caminhoes).max().getAsInt();
+        int minKm = Arrays.stream(caminhoes).min().getAsInt();
+        int diferenca = maxKm - minKm;
+
+        return diferenca < minDiferenca;
+    }
+
     public static void main(String[] args) {
-        int[] rotas = {35, 34, 33, 23, 21, 32, 35, 19, 26, 42};
-        Backtracking bt = new Backtracking(rotas, 3);
-        bt.distribuir();
+        int numCaminhoes = 3;
+        int[] rotas = {32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27};
+        Backtracking backtracking = new Backtracking(numCaminhoes, rotas);
+        backtracking.distribuirRotas();
     }
 }
