1223f371d7d2d994245d597a1f676bbcc0acb896
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2023-12-15 00:13:36 -0300

    algoritmo corrigido programcao dinamica

--- ProgramacaoDinamica/ProgramacaoDinamica.java
+++ ProgramacaoDinamica/ProgramacaoDinamica.java
@@ -1,53 +1,81 @@
 package ProgramacaoDinamica;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ProgramacaoDinamica {
 
     public static int turn = 0;
+    public static int totalKm = 0;
 
-    public static void execute(int[] routes, List<List<Integer>>[] trucks){
+    public static void execute(List<Integer> routes, List<Integer>[] trucks){
 
-        int[][] memoryTable = new int[routes.length][trucks[0].size()];
+        
 
-        //filling the table with invalid values
+        int bestResult = (int) Math.ceil((double) totalKm / trucks.length);
+
+        int[][] memoryTable = new int[bestResult + 1][routes.size() + 1];
+
+        // filling the table with invalid values
         for (int[] row : memoryTable) {
             Arrays.fill(row, -1);
         }
 
-        Arrays.sort(routes);
-        ProgramacaoDinamica.distributeRoutes(routes, routes.length - 1, trucks, memoryTable);
+        for (int i = 0; i <= routes.size(); i++) {
+            memoryTable[0][i] = 1;
+        }
 
-    }
-
-    public static void distributeRoutes(int[] routes, int actualRoute, List<List<Integer>>[] trucks, int[][] memoryTable) {
-        if (actualRoute >= 0) {
-
-            int smallestSet = Integer.MAX_VALUE;
-            List<Integer> iterationTruckSetRef = null;
-            List<Integer> smallestTruckSetReference = null;
-
-            for (int i = 0; i < trucks.length; i++) {
-                iterationTruckSetRef = trucks[i].get(ProgramacaoDinamica.turn);
-                int totalKm = iterationTruckSetRef.stream().reduce(0, (accumulator, route) -> accumulator += route);
-
-                if (totalKm < smallestSet) {
-                    smallestSet = totalKm;
-                    smallestTruckSetReference = iterationTruckSetRef;
+        for (int i = 1; i <= bestResult; i++) {
+            for (int j = 1; j <= routes.size(); j++) {
+                memoryTable[i][j] = memoryTable[i][j - 1];
+                if (i >= routes.get(j - 1)) {
+                    if (memoryTable[i][j] == 1 || memoryTable[i - routes.get(j - 1)][j - 1] == 1) {
+                        memoryTable[i][j] = 1;
+                    } else {
+                        memoryTable[i][j] = 0;
+                    }
                 }
             }
+        }
 
-            smallestTruckSetReference.add(routes[actualRoute]);
 
-            // verify if the next solution was already finded
-            if (memoryTable[actualRoute][ProgramacaoDinamica.turn] == -1) {
+        //!TODO: remover as rotas que já foram selecionadas.
+        // System.out.println("Caminhão" + turn);
+        List<Integer> result = ProgramacaoDinamica.findRouteSet(routes, memoryTable);
+        trucks[turn] = result;
 
-                // find the next solution and save the finded solution
-                distributeRoutes(routes, actualRoute - 1, trucks, memoryTable);
-                memoryTable[actualRoute][ProgramacaoDinamica.turn] = smallestSet;
+        for (Integer route : result) {
+            routes.remove(routes.indexOf(route));
+        }
+        // ProgramacaoDinamica.turn++;
 
+    }
+
+
+    public static List<Integer> findRouteSet(List<Integer> routes, int[][] table){
+
+        List<Integer> findedSet = new ArrayList<Integer>();
+        int line = table.length - 1;
+        int column = routes.size();
+
+        for(int i = line; i > 0; i--){
+            if (table[i][column] == 1) {
+                line = i;
+                break;
             }
         }
+
+        while (line > 0 && column > 0) {
+            if (table[line][column] != table[line][column - 1]) {
+                findedSet.add(routes.get(column - 1));
+                line -= routes.get(column - 1);
+            }
+            column--;
+        }
+        
+
+        return findedSet;
     }
+
 }
