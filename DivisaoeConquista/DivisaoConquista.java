78ef31327d2417353a7c4ef985cfbf3adfdf1004
Gabriel Estevão <gensobrinho@hotmail.com>
2023-12-15 01:31:19 -0300

    chore: documentação de código

--- DivisaoeConquista/DivisaoConquista.java
+++ DivisaoeConquista/DivisaoConquista.java
@@ -1,4 +1,8 @@
 import java.util.*;
+
+/**
+ * Classe DivisaoConquista para resolver o problema de distribuição de rotas para caminhões.
+ */
 public class DivisaoConquista {
 
     public static void main(String[] args) {
@@ -9,14 +13,17 @@ public class DivisaoConquista {
         long tempo = 0;
         DivisaoConquista dc = new DivisaoConquista();
 
+//      Para executar os testes 1 ou 2, é necessário descomentar uma das linhas abaixo e depois descomentar o bloco
+//      de código comentado entre as linhas 18 e 38, e comentar o bloco de código da linha 27 até 48
+
         long inicio = System.nanoTime();
-//        int[] r = {40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28};
+//      int[] r = {40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28};
         int[] r = {32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27};
         dc.dvc(r,3);
         long fim = System.nanoTime();
         System.out.println("Tempo de execução: " + (fim - inicio) + " nanossegundos.");
 
-
+        // Bloco de código para teste de 30 segundos.
 //        while (true) {
 //
 //            long inicio = System.currentTimeMillis();
@@ -41,6 +48,14 @@ public class DivisaoConquista {
 //        }
 
     }
+
+    /*
+     * Método responsável por ordenar as rotas, calcular a média das distâncias e gerar dois conjuntos que armazenarão
+     * as rotas atribuidas a cada caminhão.
+     *
+     * @params rotas - Conjunto de rotas a ser distribuido
+     * @params numCaminhoes - Quantidade de caminhÕes que receberão a distribuição das rotas
+     */
     public static void dvc(int[] rotas, int numCaminhoes) {
 
         Arrays.sort(rotas);
@@ -61,6 +76,18 @@ public class DivisaoConquista {
 
     }
 
+    /* Metódo recursivo responsável por dividir as rotas entre os caminhões.
+     *
+     * @params rotas - rotas a serem distribuidas
+     * @params inicio - indice para identificar o inicio do subconjunto a ser gerado
+     * @params fim - indice para identificar o fim do subconjunto a ser gerado
+     * @params kmCaminhoes - Conjunto responsável por armazenar as distâncias percorridas por cada caminhão até o momento.
+     * @params media - Média da quilometragem total das rotas sobre o número de caminhões para servir de limite máximo
+     *  da soma de cada subconjunto.
+     * @params rotasPorCaminhao - Conjunto responsável por armazenar as rotas distribuidas até o momento
+     * @params caminhaoAtual - Indice para identificar o caminhão que está recebendo as rotas.
+     *
+     */
     public static void dividir(int[] rotas, int inicio, int fim, int[] kmCaminhoes, double media, List<Integer>[] rotasPorCaminhao, int caminhaoAtual) {
         if (caminhaoAtual >= kmCaminhoes.length) {
             return;
--- DivisaoeConquista/DivisaoEConquista2.java
+++ DivisaoeConquista/DivisaoEConquista2.java
@@ -8,6 +8,10 @@ public class DivisaoEConquista2 {
         int numCaminhoes = 3;
         double dispersao = 0.5;
 
+
+//      Para executar os testes 1 ou 2, é necessário descomentar uma das linhas abaixo e depois descomentar o bloco
+//      de código comentado entre as linhas 18 e 38, e comentar o bloco de código da linha 43 até 76
+
         //int[] rotas = {40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28};
         //int[] rotas = {32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27};
 
@@ -34,6 +38,8 @@ public class DivisaoEConquista2 {
 
         long startTime = System.nanoTime();
 
+
+        // Código referente ao Teste 3 apresentado no relatório.
         while (true) {
             List<int[]> conjuntosDeTeste = GeradorDeProblemas.geracaoDeRotas(qtdRotas, 10, dispersao);
             for (int[] rotas : conjuntosDeTeste) {
@@ -73,6 +79,10 @@ public class DivisaoEConquista2 {
     }
 
 
+    /*
+     * Método recursivo que divide um array de inteiros em dois subarrays.
+     * @params rotas - Conjunto de rotas a ser dividido e distribuido futuramente.
+     */
     public static int[] divideConquista(int[] rotas) {
         if (rotas.length == 1) {
             return rotas;
@@ -86,6 +96,12 @@ public class DivisaoEConquista2 {
         return combinar(rotas1, rotas2);
     }
 
+    /* Método responsável por criar um novo array combinando os dois subarrays anteriormente divididos.
+     *
+     * @params rotas1 - Subconjunto dividido anteriormente representando a metade esquerda de um conjunto original
+     * @params rotas2 - Subconjunto dividido anteriormente representando a metade direita de um conjunto original
+     * @return rotas - rotas distribuidas balanceadamente entre os caminhões.
+     */
     public static int[] combinar(int[] rotas1, int[] rotas2) {
         int[] rotas = new int[rotas1.length + rotas2.length];
 
