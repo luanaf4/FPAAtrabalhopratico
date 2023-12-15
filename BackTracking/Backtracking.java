1d1422abb08af907fdc67243744bd830fbe9545a
luanaf4 <luana.fleury4@gmail.com>
2023-12-14 22:55:44 -0300

    implementação do tempo de execução em nanosegundos e milisegundos

--- BackTracking/Backtracking.java
+++ BackTracking/Backtracking.java
@@ -14,7 +14,6 @@ public class Backtracking {
     private int menorDiferenca = Integer.MAX_VALUE;
     private int quilometragemIdeal;
 
-
     public Backtracking(int numCaminhoes, int[] rotas) {
         this.numCaminhoes = numCaminhoes;
         this.rotas = rotas;
@@ -25,11 +24,9 @@ public class Backtracking {
         this.quilometragemIdeal = Arrays.stream(rotas).sum() / numCaminhoes;
     }
 
-
     public void distribuirRotas() {
         distribuirRotas(0);
     }
-
 
     private void distribuirRotas(int rotaAtual) {
         if (rotaAtual == rotas.length) {
@@ -56,29 +53,31 @@ public class Backtracking {
         }
     }
 
+    public static void main(String[] args) {
+        int numCaminhoes = 3;
+        int[] rotas = {40, 36, 38, 29, 32, 28, 31, 35, 31, 30, 32, 30, 29, 39, 35, 38, 39, 35, 32, 38, 32, 33, 29, 33, 29, 39, 28};
 
-//    * Conjunto de rotas 1: 40;36;38;29;32;28;31;35;31;30;32;30;29;39;35;38;39;35;32;38;32;33;29;33;29;39;28
+        Instant start = Instant.now();
+        Backtracking backtracking = new Backtracking(numCaminhoes, rotas);
+        backtracking.distribuirRotas();
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
 
-//    public static void main(String[] args) {
-//        int numCaminhoes = 3;
-//        int[] rotas = {40, 36, 38, 29, 32, 28, 31, 35, 31, 30, 32, 30, 29, 39, 35, 38, 39, 35, 32, 38, 32, 33, 29, 33, 29, 39, 28};
-//
-//        Backtracking backtracking = new Backtracking(numCaminhoes, rotas);
-//        backtracking.distribuirRotas();
-//
-//        System.out.println("Melhor distribuição: " + Arrays.toString(backtracking.melhorDistribuicao));
-//        for (int i = 0; i < numCaminhoes; i++) {
-//            System.out.println("Caminhão " + (i + 1) + ": " + backtracking.melhorDistribuicao[i] + " km");
-//            System.out.print("Rotas: ");
-//            for (int j = 0; j < rotas.length; j++) {
-//                if (backtracking.melhorRotasCaminhoes[i][j] == 1) {
-//                    System.out.print(rotas[j] + " ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
+        System.out.println("Melhor distribuição: " + Arrays.toString(backtracking.melhorDistribuicao));
+        for (int i = 0; i < numCaminhoes; i++) {
+            System.out.println("Caminhão " + (i + 1) + ": " + backtracking.melhorDistribuicao[i] + " km");
+            System.out.print("Rotas: ");
+            for (int j = 0; j < rotas.length; j++) {
+                if (backtracking.melhorRotasCaminhoes[i][j] == 1) {
+                    System.out.print(rotas[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Tempo de execução: " + duration.toMillis() + " ms / " + duration.toNanos() + " ns");
+    }
+}
 
 
 //    Conjunto de rotas 2: 32;51;32;43;42;30;42;51;43;51;29;25;27;32;29;55;43;29;32;44;55;29;53;30;24;27
@@ -108,42 +107,42 @@ public class Backtracking {
 // Utilizando o código do gerador de problemas
 
 
-    public static void main(String[] args) {
-        int numCaminhoes = 3;
-        int tamConjunto = 10;
-        double dispersao = 0.5;
-        int quantRotas = 6; // começando com 6 rotas
-
-
-        while (true) {
-            List<int[]> conjuntosDeTeste = GeradorDeProblemas.geracaoDeRotas(quantRotas, tamConjunto, dispersao);
-            long totalDuration = 0;
-
-
-            for (int[] rotas : conjuntosDeTeste) {
-                Instant start = Instant.now();
-                Backtracking backtracking = new Backtracking(numCaminhoes, rotas);
-                backtracking.distribuirRotas();
-                Instant end = Instant.now();
-                Duration duration = Duration.between(start, end);
-                totalDuration += duration.toMillis();
-
-                // Imprimir as rotas
-                System.out.println("Rotas: " + Arrays.toString(rotas));
-            }
-
-            long averageDuration = totalDuration / tamConjunto;
-
-            System.out.println("Número de rotas: " + quantRotas + ", Tempo médio de execução: " + averageDuration + " ms");
-
-            if (averageDuration > 30000) { // se o tempo médio de execução ultrapassar 30 segundos, pare o loop
-                break;
-            }
-
-            quantRotas++; // aumenta o número de rotas para o próximo teste
-
-        }
-
-
-    }
-}
+//    public static void main(String[] args) {
+//        int numCaminhoes = 3;
+//        int tamConjunto = 10;
+//        double dispersao = 0.5;
+//        int quantRotas = 6; // começando com 6 rotas
+//
+//
+//        while (true) {
+//            List<int[]> conjuntosDeTeste = GeradorDeProblemas.geracaoDeRotas(quantRotas, tamConjunto, dispersao);
+//            long totalDuration = 0;
+//
+//
+//            for (int[] rotas : conjuntosDeTeste) {
+//                Instant start = Instant.now();
+//                Backtracking backtracking = new Backtracking(numCaminhoes, rotas);
+//                backtracking.distribuirRotas();
+//                Instant end = Instant.now();
+//                Duration duration = Duration.between(start, end);
+//                totalDuration += duration.toMillis();
+//
+//                // Imprimir as rotas
+//                System.out.println("Rotas: " + Arrays.toString(rotas));
+//            }
+//
+//            long averageDuration = totalDuration / tamConjunto;
+//
+//            System.out.println("Número de rotas: " + quantRotas + ", Tempo médio de execução: " + averageDuration + " ms");
+//
+//            if (averageDuration > 30000) { // se o tempo médio de execução ultrapassar 30 segundos, pare o loop
+//                break;
+//            }
+//
+//            quantRotas++; // aumenta o número de rotas para o próximo teste
+//
+//        }
+//
+//
+//    }
+//}
