015d6817368acaeb6b91971021f2b0702e8b84b5
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2023-12-15 03:49:12 -0300

    final

--- ProgramacaoDinamica/App.java
+++ ProgramacaoDinamica/App.java
@@ -12,56 +12,75 @@ public class App {
     public static void main(String[] args) {
 
         int trucksQuantity = 3;
-        int routesQuantity = 12;
+        int routesQuantity = 22;
         int setLength = 10;
         long totalTime = 0;
 
-        // List<int[]> routesSet = GeradorDeProblemas.geracaoDeRotas(routesQuantity, setLength, 0.5);
-        List<int[]> routesSet = new ArrayList<>();
-        int[] set1 = new int[]{40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28};
-        int[] set2 = new int[]{32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27};
+        // List<int[]> routesSet = new ArrayList<>();
+        // int[] set1 = new
+        // int[]{40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28};
+        // int[] set2 = new
+        // int[]{32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27};
 
-        routesSet.add(set1);
-        routesSet.add(set2);
+        // routesSet.add(set1);
+        // routesSet.add(set2);
 
         // array de quantidadde de caminhoes que possui uma lista de soluções para os
         // conjuntos de rotas
         List<Integer>[] trucks = new ArrayList[trucksQuantity];
 
-        long start = System.currentTimeMillis();
+        for (int x = 1; x <= 10; x++) {
 
-        for (int[] routes : routesSet) {
+            List<int[]> routesSet = GeradorDeProblemas.geracaoDeRotas(routesQuantity * x, setLength, 0.5);
 
-            List<Integer> routesAsList = new ArrayList<Integer>(Arrays.asList(Arrays.stream(routes).boxed().toArray(Integer[]::new)));
+            totalTime = 0;
 
-            ProgramacaoDinamica.turn = 0;
+            for (int y = 0; y < 10; y++) {
 
-            int totalKm = 0;
-            for (int i = 0; i < routes.length; i++) {
-                totalKm += routes[i];
+                long start = System.currentTimeMillis();
+
+                for (int[] routes : routesSet) {
+
+                    List<Integer> routesAsList = new ArrayList<Integer>(
+                            Arrays.asList(Arrays.stream(routes).boxed().toArray(Integer[]::new)));
+
+                    ProgramacaoDinamica.turn = 0;
+
+                    int totalKm = 0;
+                    for (int i = 0; i < routes.length; i++) {
+                        totalKm += routes[i];
+                    }
+
+                    ProgramacaoDinamica.totalKm = totalKm;
+
+                    for (int i = 0; i < trucks.length; i++) {
+                        ProgramacaoDinamica.execute(routesAsList, trucks);
+                        ProgramacaoDinamica.turn++;
+                    }
+
+                    
+
+                    // totalTime = end - start;
+                    // System.out.println("Tempo de execução: " + totalTime + "ms");
+                    // System.out.println("Tempo de execução: " + totalTime * 1000000 + "ns");
+                    // System.out.println("---------------------------------------------------");
+
+                    for (int i = 0; i < trucks.length; i++) {
+                        System.out.println("Conjunto resultado do Caminhão " + (i + 1) + ": " + trucks[i]);
+                        int routesSum = trucks[i].stream().reduce(0, (accumulator, index) -> accumulator += index);
+                        System.out.println("Soma total das rotas: " + routesSum + "\n");
+                    }
+
+                }
+
+                long end = System.currentTimeMillis();
+                totalTime = end - start;
+
             }
 
-            ProgramacaoDinamica.totalKm = totalKm;
-
-            for (int i = 0; i < trucks.length; i++) {
-                ProgramacaoDinamica.execute(routesAsList, trucks);
-                ProgramacaoDinamica.turn++;
-            }
-
-            long end = System.currentTimeMillis();
-
-            totalTime = end - start;
-            System.out.println("Tempo de execução: " + totalTime + "ms");
-            System.out.println("Tempo de execução: " + totalTime * 1000000 + "ns");
-            System.out.println("---------------------------------------------------");
-
-
-            for (int i = 0; i < trucks.length; i++) {
-                System.out.println("Conjunto resultado do Caminhão " + (i + 1) + ": " + trucks[i]);
-                int routesSum = trucks[i].stream().reduce(0, (accumulator, index) -> accumulator += index);
-                System.out.println("Soma total das rotas: " + routesSum + "\n");
-            }
+            System.out.println("Tempo médio de execução com " + x + "T: " + totalTime * 1000000 + "ns");
 
         }
+
     }
 }
